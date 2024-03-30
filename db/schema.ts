e9f8e8fd6f0081b3d7097256cce5e42a2a446bdf
tamhid-mahmod <tamhidmahmod94@gmail.com>
2024-03-30 22:45:03 +0600

    added neon db and drizzle orm

---
+++ db/schema.ts
@@ -0,0 +1,8 @@
+import { sql } from "drizzle-orm";
+import { pgTable, uuid, text } from "drizzle-orm/pg-core";
+
+export const courses = pgTable("courses", {
+  id: uuid("id").default(sql`gen_random_uuid()`),
+  title: text("title").notNull(),
+  imageSrc: text("image_src").notNull(),
+});
